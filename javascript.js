61aff96cc42b40321d5daee438d355b768eb7ca2
varun <varunpandeyuttrakhand+github@gmail.com>
2023-07-19 17:40:11 +0530

    updated

--- javascript.js
+++ javascript.js
@@ -704,11 +704,13 @@ const setStickyMob = () => {
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.bottom > 0 &&
+        rect.top < window.innerHeight &&
+        rect.right > 0 &&
+        rect.left < window.innerWidth &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     );
 }
+
 
 // If the element is in the viewport, the function returns true. Otherwise, it returns false.
