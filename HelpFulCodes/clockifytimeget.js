f7ce8c8e0fe181d6612617741038e394e8e50406
varun <varunpandeyuttrakhand+github@gmail.com>
2023-11-04 20:31:23 +0530

    'updated'

--- HelpFulCodes/clockifytimeget.js
+++ HelpFulCodes/clockifytimeget.js
@@ -1,3 +1,25 @@
+
+const yesterdayDate = () => {
+    const date = new Date()
+    date.setDate(date.getDate() - 1)
+    return date
+}
+
+// Function to copy text to clipboard
+function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(textArea);
+}
+
 const calculateTime = (day = 1) => {
     const parentSelectors = document.querySelectorAll(`html body .cl-tracker-entries-wrapper>entry-group:nth-child(${day}) .cl-card>*:nth-child(n+2)`);
     const timeByDescription = {}; // Track time for each description
@@ -26,9 +48,18 @@ const calculateTime = (day = 1) => {
             return `${description}: ${totalHours.toFixed(2)} hours`;
         });
 
-        console.log(`Date : ${new Date()} \n\n\n` + results.join('\n') + `\n\nTotal: ${totalTime.toFixed(2)} hours`);
+        const status = `Date : ${yesterdayDate().toString()} \n\n\n` + results.join('\n') + `\n\nTotal: ${totalTime.toFixed(2)} hours`
+
+        copyTextToClipboard(status);
+
+        console.log(status);
     }
 }
 
 // Example usage
 calculateTime(); // Defaults to today (day 1)
+
+// Updates
+
+// Features :
+// auto copy all your day work from clockify just run
